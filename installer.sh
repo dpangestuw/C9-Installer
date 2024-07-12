7af9d78c264e07bb68e3702c053d5cb2fb1819be
dpangestuw <160391907+dpangestuw@users.noreply.github.com>
2024-07-12 11:57:54 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -7,7 +7,7 @@ curl -sL https://deb.nodesource.com/setup_20.x -o ~/nodesource_setup.sh
 sudo bash ~/nodesource_setup.sh
 sudo apt-get install nodejs -y
 sudo ufw allow 8080
-git clone https://github.com/c9/core.git /c9sdk
-sudo bash /c9sdk/scripts/install-sdk.sh
+git clone https://github.com/c9/core.git c9sdk
+sudo bash c9sdk/scripts/install-sdk.sh
 sudo apt-get install npm -y
 npm install forever -g
